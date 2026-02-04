6b613e6eebb63e0cb9470cfd584972364825f186
Shivamm <78911395+shivamm04u@users.noreply.github.com>
2026-02-05 01:28:03 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
-import { useAuth } from '@/AuthContext';
+import { useAuth } from '@/contexts/AuthContext'; // <--- CORRECT PATH
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
