79ef6a01f074fde6b8a15188222d73003fdb22a9
Shivamm <78911395+shivamm04u@users.noreply.github.com>
2026-02-05 00:49:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// Import the Context you just created
+import { AuthProvider } from './contexts/AuthContext' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>  {/* <--- THIS WRAPPER IS CRITICAL */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
