fef113c4f75299d8e711a4e544c9acb05faf79e9
Shivamm <78911395+shivamm04u@users.noreply.github.com>
2026-02-05 01:27:09 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // Import the Context you just created
-import { AuthProvider } from './contexts/AuthContext' 
+import { AuthProvider } from './contexts/AuthContext' // <--- CORRECT PATH 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
